7430be8a2b9b9f245be7719c17f76b7ba19cc175
Choaib Elmadi <111356633+Choaib-ELMADI@users.noreply.github.com>
2023-01-23 16:58:05 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(
+    ".swiper",
+    {
+        speed: 500,
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    },
+);
